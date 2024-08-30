cb4a441df73c797f8fea06d8b4bb729c5ebcd600
Farhaan <farhaan654@gmail.com>
2024-08-30 00:14:38 -0600

    Simple styling

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
 function About() {
   return (
-    <div className="section">
+    <div className="about-section">
 
       <div className="about-left">
         <h1>Who I Am</h1>
@@ -40,54 +40,58 @@ function About() {
 
 function Experience() {
   return(
-    <div className="section">
-
+    <>
       <h1>Experience</h1>
 
-      <div className="experience-item">
-        <h2>Position</h2>
-        <h4>Timeframe</h4>
-        <h4>Location</h4>
-        <p>What I did at the job</p>
-      </div>
+      <div className="experience-section">
 
-      <div className="experience-item">
-        <h2>Position 2</h2>
-        <h4>Timeframe</h4>
-        <h4>Location</h4>
-        <p>What I did at the job</p>
-      </div>
+        <div className="experience-item">
+          <h2>Position</h2>
+          <h4>Timeframe</h4>
+          <h4>Location</h4>
+          <p>What I did at the job</p>
+        </div>
 
-    </div>
+        <div className="experience-item">
+          <h2>Position 2</h2>
+          <h4>Timeframe</h4>
+          <h4>Location</h4>
+          <p>What I did at the job</p>
+        </div>
+
+      </div>
+    </>
   )
 }
 
 
 function Projects() {
   return(
-    <div className="section">
-
+    <>
       <h1>Projects</h1>
 
-      <div className="project-item">
-        <h3>Project</h3>
-        <p>Description of the project</p>
-        <ul>
-          <li>Tech 1</li>
-          <li>Tech 2</li>
-        </ul>
-      </div>
+      <div className="projects-section">
 
-      <div className="project-item">
-        <h3>Project 2</h3>
-        <p>Description of the project</p>
-        <ul>
-          <li>Tech 1</li>
-          <li>Tech 2</li>
-        </ul>
-      </div>
+        <div className="project-item">
+          <h3>Project</h3>
+          <p>Description of the project</p>
+          <ul>
+            <li>Tech 1</li>
+            <li>Tech 2</li>
+          </ul>
+        </div>
 
-    </div>
+        <div className="project-item">
+          <h3>Project 2</h3>
+          <p>Description of the project</p>
+          <ul>
+            <li>Tech 1</li>
+            <li>Tech 2</li>
+          </ul>
+        </div>
+
+      </div>
+    </>
 
 
 
