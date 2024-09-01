a77c351764595f6e3a889c9da90e8a2da62a9db7
Farhaan <farhaan654@gmail.com>
2024-09-01 03:13:12 -0600

    Added actual data

--- src/App.jsx
+++ src/App.jsx
@@ -41,17 +41,23 @@ function Experience() {
       <div className="experience-section">
 
         <div className="experience-item">
-          <h2>Position</h2>
-          <h4>Timeframe</h4>
-          <h4>Location</h4>
-          <p>What I did at the job</p>
+          <h2>Junior Developer Intern</h2>
+          <h4>Interface Technologies Pvt Ltd | May 2023 - August 2023</h4>
+          <p>
+            As a Junior Developer Intern at Interface Technologies,
+            I contributed to web and mobile app development, collaborated 
+            on projects, and assisted in testing and maintaining applications.
+          </p>
         </div>
 
         <div className="experience-item">
-          <h2>Position 2</h2>
-          <h4>Timeframe</h4>
-          <h4>Location</h4>
-          <p>What I did at the job</p>
+          <h2>Information Technology Support</h2>
+          <h4>Punjab National Bank | June 2022 - August 2022</h4>
+          <p>
+          Assisted in setting up and configuring office computers, 
+          managed existing systems for optimal performance, and provided 
+          IT support by troubleshooting and resolving technical issues.
+          </p>
         </div>
 
       </div>
@@ -70,29 +76,48 @@ function Projects() {
       <div className="projects-section">
 
         <div className="project-item">
-          <h2>Project</h2>
-          <p>Description of the project</p>
+          <h2>Controller Tester</h2>
+          <p>
+            Developed a Python application using Pygame and PyQt 
+            to test the functionality of game controllers, 
+            featuring a user-friendly GUI for easy interaction 
+            and real-time feedback
+          </p>
           <ul>
-            <li>Tech 1</li>
-            <li>Tech 2</li>
+            <li>Python</li>
+            <li>Pygame</li>
+            <li>PyQt</li>
           </ul>
         </div>
 
         <div className="project-item">
           <h2>Project 2</h2>
-          <p>Description of the project</p>
+          <p>
+            A web application that helps users decide what to play next 
+            by presenting a random game from their Steam library based 
+            on their profile URL.
+          </p>
           <ul>
-            <li>Tech 1</li>
-            <li>Tech 2</li>
+            <li>React</li>
+            <li>JavaScript</li>
+            <li>Steam API</li>
+            <li>HTML</li>
+            <li>CSS</li>
           </ul>
         </div>
 
         <div className="project-item">
-          <h2>Project 3</h2>
-          <p>Description of the project</p>
+          <h2>Personal Portfolio</h2>
+          <p>
+            The page that you are on!
+            I built this to showcase my experience and projects.
+            Building this helped me get familiar with the React framework.
+          </p>
           <ul>
-            <li>Tech 1</li>
-            <li>Tech 2</li>
+            <li>React</li>
+            <li>JavaScript</li>
+            <li>HTML</li>
+            <li>CSS</li>
           </ul>
         </div>
 
